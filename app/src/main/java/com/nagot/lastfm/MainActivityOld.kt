415816eee0a27f4513caeee224f1706f94e3ddaa
Ian Nagot <i.nagot@gamahealthcare.com>
2018-03-16 10:21:05 +0000

    Initial commit

---
+++ app/src/main/java/com/nagot/lastfm/MainActivityOld.kt
@@ -0,0 +1,14 @@
+package com.nagot.lastfm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivityOld : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/com/nagot/lastfm/ui/main/MainActivity.java
@@ -0,0 +1,18 @@
+package com.nagot.lastfm.ui.main;
+
+import android.os.Bundle;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by IanNagot on 16/03/2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
